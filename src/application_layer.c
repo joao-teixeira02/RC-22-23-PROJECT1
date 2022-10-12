ae038f851075bcf4f1614ea1bac1e9619085c3a4
pmoreira01 <pedro2001moreira@gmail.com>
2022-10-12 10:30:55 +0100

    Work!

--- src/application_layer.c
+++ src/application_layer.c
@@ -9,11 +9,23 @@ void applicationLayer(const char *serialPort, const char *role, int baudRate,
 {
 
     LinkLayer connectionparameters;
-    connectionparameters.role = role;
+    printf("role :%s\n", role);
+    if (!strcmp(role, "tx")) {
+        connectionparameters.role = 0;
+    }
+    else if (!strcmp(role, "rx")) {
+        connectionparameters.role = 1;
+    }
+    else {
+        printf("Error in role\n");
+        exit(-1);
+    }
     connectionparameters.baudRate = baudRate;
     *connectionparameters.serialPort = serialPort; 
     connectionparameters.nRetransmissions = nTries;
     connectionparameters.timeout = timeout;
+
+    printf("filename: %s\n", filename);
 
     int fd = llopen(connectionparameters);
 
--- src/link_layer.c
+++ src/link_layer.c
@@ -20,12 +20,12 @@
 
 #define BUF_SIZE 256
 #define FLAG 0x7E
-#define SET 0x03
-#define UA 0x07
 #define TRANSMITTER_COMMAND 0x03
-#define RECEIVER_REPLY 0x03
-#define RECEIVER_COMMAND 0x01
 #define TRANSMITTER_REPLY 0x01
+#define RECEIVER_COMMAND 0x01
+#define RECEIVER_REPLY 0x03
+#define CONTROL_SET 0x03
+#define CONTROL_UA 0x07
 #define SU_BUF_SIZE 5
 
 #define TIMEOUT_SECS 3
@@ -49,6 +49,69 @@ int timeout_count = 0;
 struct termios oldtio;
 struct termios newtio;
 
+void stateMachine(State * state, unsigned char byte[], LinkLayer connectionParameters)
+{
+
+    unsigned char A;
+    unsigned char C;
+
+    if (connectionParameters.role == 0) {
+        unsigned char A = TRANSMITTER_COMMAND;
+        unsigned char C = CONTROL_SET;
+    }
+    else if (connectionParameters.role == 1) {
+        unsigned char A = RECEIVER_REPLY;
+        unsigned char C = CONTROL_UA;
+    }
+    else {
+        printf("Undefined role in state machine\n");
+        exit(-1);
+    }
+
+    switch(*state){
+    case StateSTART:
+        if(byte == FLAG)
+            *state = StateFLAG;
+        
+        break;
+    case StateFLAG:
+        if(byte == FLAG)
+            *state = StateFLAG;
+        else if(byte == A)
+            *state = StateA;
+        else
+            *state = StateSTART;
+        
+        break;
+    case StateA:
+        if(byte == FLAG)
+            *state = StateFLAG;
+        else if(byte == C)
+            *state = StateC;
+        else
+            *state = StateSTART;
+        
+        break;
+    case StateC:
+        if(byte == FLAG)
+            *state = StateFLAG;
+        else if(byte == (A^C))
+            *state = StateBCC;
+        else
+            *state = StateSTART;
+        
+        break;
+        
+    case StateBCC:
+        if(byte == FLAG)
+            *state = StateSTOP;
+        else
+            *state = StateSTART;
+        
+        break;
+    }
+}
+
 // Alarm function handler
 void alarmHandler(int signal)
 {
@@ -63,6 +126,7 @@ void alarmHandler(int signal)
 ////////////////////////////////////////////////
 int llopen(LinkLayer connectionParameters)
 {
+    unsigned char buf;
 
     int fd = open(connectionParameters.serialPort, O_RDWR | O_NOCTTY);
     if (fd < 0)
@@ -110,24 +174,31 @@ int llopen(LinkLayer connectionParameters)
 
     printf("New termios structure set\n");
 
-    if(role == "tx"){
+    if(connectionParameters.role == 0){
         (void)signal(SIGALRM, alarmHandler);
         (void)siginterrupt(SIGALRM,TRUE); //system call interrupted by alarm isn't restarted
         
+        unsigned char SET_packet[SU_BUF_SIZE] = {0}; 
+
+        SET_packet[0] = FLAG;
+        SET_packet[1] = TRANSMITTER_COMMAND;
+        SET_packet[2] = CONTROL_SET;
+        SET_packet[3] = (TRANSMITTER_COMMAND ^ CONTROL_SET);
+        SET_packet[4] = FLAG;
+
         while (state != StateSTOP && timeout_count < MAX_TIMEOUTS)
         {
 
             if (alarm_enabled == FALSE)
             {
-                llwrite(fd, out_char, 1);
+                llwrite(fd, &SET_packet, 1);
                 alarm(TIMEOUT_SECS); // Set alarm
                 alarm_enabled = TRUE;
             }
 
             // Returns after 1 chars has been input
-            llread(fd, &in_char);        
-            stateMachine(&state, in_char);
-
+            llread(fd, &buf);        
+            stateMachine(&state, &buf, connectionParameters);
         }
 
         alarm(0);
@@ -140,78 +211,37 @@ int llopen(LinkLayer connectionParameters)
             printf("Connection established\n");
         }
 
-    }else if (role == "rx"){
+    }else if (connectionParameters.role == 1){
 
         while (state != StateSTOP)
         {
             // Returns after 1 chars has been input
-            llread(fd, &out_char);        
-            stateMachine(&state, out_char);
+            llread(fd, buf);        
+            stateMachine(&state, &buf, connectionParameters);
         }
         
         printf("Received set up frame\n");
         
-        unsigned char bcc[5] = {1,2,3,4,5};
+        unsigned char UA_packet[SU_BUF_SIZE] = {0};
         
-        llwrite(fd, bcc, 5);
+        UA_packet[0] = FLAG;
+        UA_packet[1] = RECEIVER_REPLY;
+        UA_packet[2] = CONTROL_UA;
+        UA_packet[3] = (RECEIVER_REPLY^CONTROL_UA);
+        UA_packet[4] = FLAG;
+        
+        llwrite(fd, UA_packet, SU_BUF_SIZE);
         printf("Sent unnumbered acknowledgment frame\n");
 
         // Wait until all bytes have been written to the serial port
         sleep(1);
         printf("Connection established\n");
+    } else {
+        printf("Undefined role passed as argument to llopen()\n");
+        exit(-1);
     }
 
     return fd;
-}
-
-void stateMachine(State * state, unsigned char byte[])
-{
-
-    unsigned char A = RECEIVER_REPLY;
-    unsigned char C = UA;
-    
-    switch(*state){
-    case StateSTART:
-        if(byte == FLAG)
-            *state = StateFLAG;
-        
-        break;
-    case StateFLAG:
-        if(byte == FLAG)
-            *state = StateFLAG;
-        else if(byte == A)
-            *state = StateA;
-        else
-            *state = StateSTART;
-        
-        break;
-    case StateA:
-        if(byte == FLAG)
-            *state = StateFLAG;
-        else if(byte == C)
-            *state = StateC;
-        else
-            *state = StateSTART;
-        
-        break;
-    case StateC:
-        if(byte == FLAG)
-            *state = StateFLAG;
-        else if(byte == (A^C))
-            *state = StateBCC;
-        else
-            *state = StateSTART;
-        
-        break;
-        
-    case StateBCC:
-        if(byte == FLAG)
-            *state = StateSTOP;
-        else
-            *state = StateSTART;
-        
-        break;
-    }
 }
 
 ////////////////////////////////////////////////
