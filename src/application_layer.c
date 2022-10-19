8d3a207024c72cb07641687de37280c4c77dc5d1
pmoreira01 <pedro2001moreira@gmail.com>
2022-10-19 19:34:29 +0100

    work on control packet

--- src/application_layer.c
+++ src/application_layer.c
@@ -4,6 +4,7 @@
 #include "link_layer.h"
 #include <signal.h>
 #include <stdio.h>
+#include <math.h>
 
 #define CONTROL_DATA 0x01
 #define CONTROL_START 0x02
@@ -44,18 +45,58 @@ void applicationLayer(const char *serialPort, const char *role, int baudRate,
     }
 
     fclose(in);
-    fclose(out);*/
+    fclose(out); */
 
     fseek(in, 0L, SEEK_END);
-    int sz = ftell(in);
+    long int sz = ftell(in);
     fseek(in, 0L, SEEK_SET);
 
-    printf("Tamanho do ficheiro: %d\n", sz);
+    fclose(in);
 
-    char control_packet[4];
+    int size_counter = 1;
+    long int size = sz;
+    while(size >>= 1){
+        size_counter++;
+    }
+
+    char byte_size;
+    char size_in_bytes[size_counter];
+
+    double result1;
+
+    result1 = (double) size_counter / 8.0;
+
+    double result = ceil(result);
+
+    printf("Result: %f\n", result);
+
+    printf("Tamanho do ficheiro: %ld bytes\n", sz);
+    printf("Tamanho do Tamanho do fichero: %d bits\n", size_counter);
+
+    sprintf(&byte_size, "%x", result);
+    printf("Byte size: %c\n", byte_size);
+    sprintf(&size_in_bytes, "%x", sz);
+    printf("size in bytes: %s\n", size_in_bytes);
+
+    char control_packet[7];
+
     control_packet[0] = CONTROL_START;
     control_packet[1] = TYPE_SIZE;
-    //control_packet[2] = 
+    control_packet[2] = byte_size;
 
+    int x = 0;
+
+    for (int i = 3; i < 7; i++) {
+        control_packet[i] = size_in_bytes[x];
+        x++;
+    }
+
+    printf("%x\n", control_packet[0]);
+    printf("%x\n", control_packet[1]);
+    printf("%x\n", control_packet[2]);
+    printf("%x\n", control_packet[3]);
+    printf("%x\n", control_packet[4]);
+    printf("%x\n", control_packet[5]);
+    printf("%x\n", control_packet[6]);
 
 }
