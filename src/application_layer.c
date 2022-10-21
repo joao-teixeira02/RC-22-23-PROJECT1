daf433cfaa79058997550b8d743d04dacfad4155
pmoreira01 <pedro2001moreira@gmail.com>
2022-10-21 14:54:56 +0100

    Control packet done

--- src/application_layer.c
+++ src/application_layer.c
@@ -48,55 +48,39 @@ void applicationLayer(const char *serialPort, const char *role, int baudRate,
     fclose(out); */
 
     fseek(in, 0L, SEEK_END);
-    long int sz = ftell(in);
+    long int file_size = ftell(in);
     fseek(in, 0L, SEEK_SET);
 
     fclose(in);
 
-    int size_counter = 1;
-    long int size = sz;
-    while(size >>= 1){
-        size_counter++;
+    int size_in_bits = 1;
+    long int temp = file_size;
+    while(temp >>= 1){
+        size_in_bits++;
     }
 
-    char byte_size;
-    char size_in_bytes[size_counter];
+    int size_in_bytes = (size_in_bits + (8 - 1)) / 8;
 
-    double result1;
-
-    result1 = (double) size_counter / 8.0;
-
-    double result = ceil(result);
-
-    printf("Result: %f\n", result);
-
-    printf("Tamanho do ficheiro: %ld bytes\n", sz);
-    printf("Tamanho do Tamanho do fichero: %d bits\n", size_counter);
-
-    sprintf(&byte_size, "%x", result);
-    printf("Byte size: %c\n", byte_size);
-    sprintf(&size_in_bytes, "%x", sz);
-    printf("size in bytes: %s\n", size_in_bytes);
-
-    char control_packet[7];
+    unsigned char control_packet[3 + size_in_bytes];
 
     control_packet[0] = CONTROL_START;
     control_packet[1] = TYPE_SIZE;
-    control_packet[2] = byte_size;
+    control_packet[2] = size_in_bytes;
 
-    int x = 0;
-
-    for (int i = 3; i < 7; i++) {
-        control_packet[i] = size_in_bytes[x];
-        x++;
+    for (int i = 0; i < size_in_bytes; i++) {
+        control_packet[3 + size_in_bytes - 1 - i] = (file_size & (0xff << (8 * i))) >> (8 * i);
     }
 
-    printf("%x\n", control_packet[0]);
-    printf("%x\n", control_packet[1]);
-    printf("%x\n", control_packet[2]);
-    printf("%x\n", control_packet[3]);
-    printf("%x\n", control_packet[4]);
-    printf("%x\n", control_packet[5]);
-    printf("%x\n", control_packet[6]);
+/*     printf("Pacote de Controlo \n");
+    printf("Campo de Controlo: %x\n", control_packet[0]);
+    printf("Tipo de parametro: %x\n", control_packet[1]);
+    printf("Tamanho do parametro: %d bytes\n", control_packet[2]);
+
+    for(int x = 0; x < size_in_bytes; x++){
+        printf("Tamanho do ficheiro %d: %x\n", x, control_packet[3 + x]);
+    } */
+
+    
+
 
 }
